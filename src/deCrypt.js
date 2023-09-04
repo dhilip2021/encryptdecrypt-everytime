9d811fc4747c0602c02d770597af57aeb0e12a92
Dhilip React Js Developer <88287764+dhilip2021@users.noreply.github.com>
2023-09-04 17:35:15 +0530

    Update deCrypt.js

--- src/deCrypt.js
+++ src/deCrypt.js
@@ -1,10 +1,18 @@
-function decodeWithKey(salt) {
+module.exports.urlDecoder = (salt,encoded) => {
+    // const salt ="mysecretekey";
+    try{
     const textToChars = text => text.split('').map(c => c.charCodeAt(0));
     const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
-    return encoded => encoded.match(/.{1,2}/g)
-      .map(hex => parseInt(hex, 16))
-      .map(applySaltToChar)
-      .map(charCode => String.fromCharCode(charCode))
-      .join('');
+
+    let response = encoded.match(/.{1,2}/g).map(hex => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map(charCode => String.fromCharCode(charCode))
+    .join('');
+    return response;
+    }catch(err)
+    {
+      return {error : err,message : "Invalid Secret Key"}
+    }
+    
+    
 }
-export const urlDecoder = decodeWithKey('mySecretSalt')
